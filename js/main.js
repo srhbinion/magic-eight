7125bc3d63a278d9ee50aa82e11c94139f21a102
vincent.bright@outlook.com <vincentedwardbright@gmail.com>
2016-02-17 15:49:01 -0600

    made code self-documenting

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Randomizes answers on Click 
+
 $('.magicE').on('click', function () {
     var answer = ["Yes.", "No.", "Ask me later.", "Outlook not so good.", "Don't leave your house today, you don't want to know.", "Can I tell you later?", "Can you repeat that I didn't hear you.", "No (En Espanol)."];
     var randomValue = answer[Math.floor(answer.length * Math.random())];
