ce2f3538b1ad655a8f6cd049a1f70313dcc0e5a7
Vincent Bright <vincent.bright@outlook.com>
2016-03-03 01:57:52 +0000

    Fixed 8ball generator

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-window.onload function() {
-    
+//Array of responses
+
+var answer = ["Yes.", "No.", "Ask me later.", "Outlook not so good.", "Don't leave your house today, you don't want to know.", "Can I tell you later?", "Can you repeat that I didn't hear you.", "No (En Espanol)."];
+
 //Randomizes answers on Click 
 
 $('.magicE').on('click', function () {
-    var answer = ["Yes.", "No.", "Ask me later.", "Outlook not so good.", "Don't leave your house today, you don't want to know.", "Can I tell you later?", "Can you repeat that I didn't hear you.", "No (En Espanol)."];
+
+   // Generates response 
     var randomValue = answer[Math.floor(answer.length * Math.random())];
-    $('#wisdom').html(randomValue);
+    console.log(randomValue);
+    $('.wisdom').html(randomValue);
+   
+   // Text Animation 
+   $(randomValue).fadeIn('slow', function () {
+      // Animation complete 
+   });
 });
-}; 
